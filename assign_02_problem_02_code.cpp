6dcf75c707b4383342fb5dc2b0187c059213d198
Ahmedghanem20216042 <129911978+Ahmedghanem20216042@users.noreply.github.com>
2023-04-06 20:30:05 +0200

    Update assign_02_problem_02_code.cpp

--- assign_02_problem_02_code.cpp
+++ assign_02_problem_02_code.cpp
@@ -1,3 +1,13 @@
 //Ahmed Ghanem
 //202106042
-
+import java.util.Scanner;
+ 
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(N + " * " + i + " = " + (N*i));
+        }
+    }
+}
